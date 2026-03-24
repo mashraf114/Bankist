a6200bef694d35d23e593b0cf0986c169435a5d4
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-03-24 13:53:16 +0200

    function: loan request using some & update ui method and satisfing all conditions. Status: Done

--- script.js
+++ script.js
@@ -175,6 +175,16 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -414,3 +424,14 @@ for (const account of accounts)
   if (account.owner === 'Jessica Davis') {
     // console.log(account);
   }
+
+// console.log(movements);
+const lastWithdrawal = movements.findLast(mov => mov < 0);
+// console.log(lastWithdrawal);
+
+const lastIndex = movements.findLastIndex(mov => mov > 0);
+// console.log(movements[lastIndex]);
+const anyDepo = movements.some(mov => mov > 0);
+console.log(anyDepo);
+
+//every
