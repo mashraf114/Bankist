baaa3bed5921eb565f7bb10993db62e2c63daf22
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-03-24 16:59:50 +0200

    function: sorting movements using slice to copy & sort methods, ternary operator and monitoring sorting using a state variable. Status: Done

--- script.js
+++ script.js
@@ -61,10 +61,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
-  movements.forEach(function (mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -202,6 +203,12 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
@@ -432,6 +439,180 @@ const lastWithdrawal = movements.findLast(mov => mov < 0);
 const lastIndex = movements.findLastIndex(mov => mov > 0);
 // console.log(movements[lastIndex]);
 const anyDepo = movements.some(mov => mov > 0);
-console.log(anyDepo);
+// console.log(anyDepo);
 
 //every
+// console.log(account4.movements.every(mov => mov > 0));
+
+const arr1 = [
+  [[1, 2], 3],
+  [4, [5, 6]],
+  [6, 7, 8],
+];
+// console.log(arr1.flat(2));
+
+const accountMovments = accounts.flatMap(acc => acc.movements);
+// console.log(accountMovments.reduce((acc, mov) => acc + mov));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+/*  */
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+
+const Husky = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+// console.log(Husky);
+
+// console.log(
+//   breeds.find(
+//     breed =>
+//       breed.activities.includes('fetch') && breed.activities.includes('running')
+//   ).breed
+// );
+
+// const allActivities = breeds.map(breed => breed.activities).flat();
+const allActivities = breeds.flatMap(breed => breed.activities);
+// console.log(allActivities);
+const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities);
+
+const swimmingAdjacent = [
+  ...new Set(
+    breeds
+      .filter(breed => breed.activities.includes('swimming'))
+      .flatMap(breed => breed.activities)
+      .filter(activity => activity !== 'swimming')
+  ),
+];
+// console.log(swimmingAdjacent);
+
+//  1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+// const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+// console.log(huskyWeight);
+
+// // 2. Find the name of the only breed that likes both
+// // "running" and "fetch" ("dogBothActivities" variable)
+
+// const dogBothActivities = breeds.find(
+//   breed =>
+//     breed.activities.includes('running') && breed.activities.includes('fetch')
+// ).breed;
+// console.log(dogBothActivities);
+
+// // 3. Create an array "allActivities" of all the activities
+// // of all the dog breeds
+// // const allActivities = breeds.activities.flatMap();
+// const allActivities = breeds.map(breeds => breeds.activities).flat();
+// console.log(allActivities);
+
+// // 4. Create an array "uniqueActivities" that
+// // contains only the unique activities
+// // (no activity repetitions).
+// // HINT: Use a technique with a special
+// // data structure that we studied a few sections ago.
+// const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities);
+
+// // 5. Many dog breeds like to swim.
+// // What other activities do
+// // these dogs like? Store all the
+// // OTHER activities these breeds like to do,
+// // in a unique array called "swimmingAdjacent".
+
+// // const swimmingAdjacent = uniqueActivities.filter(
+// //   activity => activity !== 'swimming'
+// // );
+
+// const swimmingAdjacent = [
+//   ...new Set(
+//     breeds.filter(breed => breed.activities.includes('swimming'))
+//     // .flatMap(breed => breed.activities)
+//     // .filter(activity => activity !== 'swimming')
+//   ),
+// ];
+// console.log(swimmingAdjacent);
+
+// // 6. Do all the breeds have an average weight
+// //  of 10kg or more? Log to the console
+// //  whether "true" or "false".
+
+// // console.log(breeds.every(breeds => breeds.averageWeight < 10));
+
+// // 7. Are there any breeds that are
+// // "active"? "Active" means that the
+// // dog has 3 or more activities.
+// // Log to the console whether "true" or
+// // "false".
+
+// console.log(breeds.some(breeds => breeds.activities.length >= 3))
+
+// //fetch
+// const fetch = breeds
+//   .filter(breed => breed.activities.includes('fetch'))
+//   .map(breed => breed.averageWeight);
+// const hev = Math.max(...fetch);
+// console.log(hev);
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+// Numbers
+// return <0 a,b
+// return >0 b,a
+console.log(movements);
+
+console.log(
+  movements.sort((a, b) => {
+    if (a > b) return 1;
+    if (b > a) return -1;
+  })
+);
+
+console.log(movements);
